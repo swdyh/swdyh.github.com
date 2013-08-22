96dadae04b0f23a947fdfc41272d405412637578
swdyh <youhei@gmail.com>
2013-08-22 13:43:16 +0900

    update imgs

--- quilt/sample_gen.rb
+++ quilt/sample_gen.rb
@@ -3,7 +3,7 @@ require 'quilt'
 num = 5
 num.times do |i|
   ic = Quilt::Identicon.new rand(1000), :scale => 10
-  out = File.join('sample', "quilt-%02d.png" % i)
+  out = File.join('sample', "quilt-%02d.png" % (i + 1))
   ic.write(out)
   p out
 end
